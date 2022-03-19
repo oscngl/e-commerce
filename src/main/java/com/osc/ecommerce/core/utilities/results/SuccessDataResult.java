285c076f8b6ce2d405b3a5591224f168fa25c060
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-03-19 16:25:25 +0300

    Results created.

---
+++ src/main/java/com/osc/ecommerce/core/utilities/results/SuccessDataResult.java
@@ -0,0 +1,22 @@
+package com.osc.ecommerce.core.utilities.results;
+
+public class SuccessDataResult<T> extends DataResult<T> {
+
+    public SuccessDataResult(T data) {
+        super(data, true);
+
+    }
+
+    public SuccessDataResult(T data, String message) {
+        super(data, true, message);
+    }
+
+    public SuccessDataResult(String message) {
+        super(null, true, message);
+    }
+
+    public SuccessDataResult() {
+        super(null, true);
+    }
+
+}
